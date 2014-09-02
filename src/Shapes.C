c65a423194e8d6be029b2351eb80b6bd9b8232f4
Nathan Mirman <nem46@cornell.edu>
2014-09-02 18:24:13 +0200

    Changed initial seeds.

--- src/Shapes.C
+++ src/Shapes.C
@@ -293,23 +293,12 @@ void Shapes::LearnGPparams( map< string, map<string, TH1D*> > & hists_ ){
    fFunc = new ROOT::Math::Functor ( this, &Shapes::GPm2llX, 4 );
    gMinuit->SetTolerance(1000.0);
    gMinuit->SetFunction( *fFunc );
-   //gMinuit->SetFixedVariable(0, "gpnorm1", 1.0);
    
-   /*
-   gMinuit->SetLowerLimitedVariable(0, "gpnorm1", 5.0, 0.1, 0.0);
-   gMinuit->SetLowerLimitedVariable(1, "gpnorm2", 10.0, 0.1, 0.0);
-   gMinuit->SetLowerLimitedVariable(2, "lx", 15, 1, 0.0);
-   gMinuit->SetLowerLimitedVariable(3, "lmass", 30, 1, 0.0);
-   */
-   
-   //gMinuit->SetFixedVariable(1, "gpnorm2", 11.8);
-   //gMinuit->SetFixedVariable(2, "lx", 13.4);
-   //gMinuit->SetFixedVariable(3, "lmass", 17.8);
-
    gMinuit->SetLowerLimitedVariable(0, "gpnorm1", 1.0, 0.1, 0.0);
-   gMinuit->SetLowerLimitedVariable(1, "gpnorm2", 10.0, 1, 0.0);
-   gMinuit->SetLowerLimitedVariable(2, "lx", 10, 1, 0.0);
-   gMinuit->SetLowerLimitedVariable(3, "lmass", 10, 1, 0.0);
+   gMinuit->SetLowerLimitedVariable(1, "gpnorm2", 10.0, 0.1, 0.0);
+   gMinuit->SetLowerLimitedVariable(2, "lx", 5, 1, 0.0);
+   gMinuit->SetLowerLimitedVariable(3, "lmass", 5, 1, 0.0);
+   
    
    gMinuit->Minimize();
    const double *xstmp = gMinuit->X();
